b27a7d23d8681a23acd838fc622b203464ec7b97
Renato-Smbr1 <renatovianacabral15@gmail.com>
2026-08-19 16:52:44 -0300

    Add files via upload

---
+++ mod2ex4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+      int num;
+      cout << "Digite um número: ";
+      cin >> num;
+
+      cout << "O triplo do número selecionado é: " << num * 3 << endl;
+     
+     return 0;
+
+}
