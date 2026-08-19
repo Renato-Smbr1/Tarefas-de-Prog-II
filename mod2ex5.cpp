5ad971d51648e5f2f39e2f25871ad1bc9a77e8e3
Renato-Smbr1 <renatovianacabral15@gmail.com>
2026-08-19 16:53:11 -0300

    Add files via upload

---
+++ mod2ex5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    
+    int num;
+    cout << "Digite um número: ";
+    cin >> num;
+    
+    int num2;
+    cout << "Digite outro número ";
+    cin >> num2;
+    
+    cout << "A soma dos números selecionados é igual a: " << num + num2 << endl;
+    
+    return 0;
+}
