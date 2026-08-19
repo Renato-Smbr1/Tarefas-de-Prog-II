c00787fab993886f87f79147c4634358a6297ce7
Renato-Smbr1 <renatovianacabral15@gmail.com>
2026-08-19 16:52:28 -0300

    Add files via upload

---
+++ mod2ex3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+      int num;
+      cout << "Digite um número: ";
+      cin >> num;
+
+      cout << "O dobro do número selecionado é: " << num * 2 << endl;
+     
+     return 0;
+
+}
