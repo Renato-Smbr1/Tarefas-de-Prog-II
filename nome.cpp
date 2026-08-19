fceceaf8d6acac78f48fe422c2f673294113ea70
Renato-Smbr1 <renatovianacabral15@gmail.com>
2026-08-19 16:51:09 -0300

    Add files via upload

---
+++ nome.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+     string nome;
+     cout << "Digite seu nome: ";
+     cin >> nome;
+  
+     cout << "Seu nome é: " << nome << endl;
+
+     return 0;
+
+}
