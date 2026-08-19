d4c3df43ae4320fb8e2dde0047f583be9e0b1bef
Renato-Smbr1 <renatovianacabral15@gmail.com>
2026-08-19 16:52:15 -0300

    Add files via upload

---
+++ mod1ex2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+      string name;
+      cout << "Digite o seu nome: ";
+      cin >> name;
+
+      string city;
+      cout << "Digite o nome da sua cidade: ";
+      cin >> city;
+
+      cout << "O seu nome é: " << name << "\n" << "O nome da sua cidade é: " << city << endl;
+
+      return 0;
+
+}
