6b2e28701a38ce0e8c9f1733f0a23f8b604776ef
Renato-Smbr1 <renatovianacabral15@gmail.com>
2026-09-23 16:45:29 -0300

    Add files via upload

---
+++ aula5ex1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int x = 10;
+    int y = 9;
+    
+    cout << "Valor de X: " << x << endl;
+    cout << "Valor de Y: " << y << endl;
+    
+    
+    
+    cout << "Endereço de memória de X: " << &x << endl;
+    cout << "Endereço de memória de Y: " << &y  << endl;
+    
+    
+    return 0;
+}
